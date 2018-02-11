f0daf80217656141c88506ba76aefc5178ec951a
wangsiyuan233 <380191957@qq.com>
2018-02-11 15:44:17 +0800

    22

--- cv.js
+++ cv.js
@@ -1,1 +1,16 @@
 
+    all.onclick = function(){
+        portfoliabar.className = 'bar state-1'
+    }
+    vallina.onclick = function(){
+        portfoliabar.className = 'bar state-2'
+    }
+    frameworks.onclick = function(){
+        portfoliabar.className = 'bar state-3'
+    }
+
+
+// Loading
+setTimeout(function(){
+    sitewelcome.classList.remove('active')
+},1000)
