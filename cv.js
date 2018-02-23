8814ac04b2b55a78858dea7c157b6d0c3d9c8a20
wangsiyuan233 <380191957@qq.com>
2018-02-23 10:42:49 +0800

    缓动

--- cv.js
+++ cv.js
@@ -42,6 +42,12 @@ for(let i = 0;i < liTags.lenght; i++ ){
 
 //跳转定位
 let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 for(let i = 0;i < aTags.lenght; i++ ){
     aTags[i].onclick = function(x){
         x.preventDefault()
@@ -50,19 +56,20 @@ for(let i = 0;i < aTags.lenght; i++ ){
         let element = document.querySelector(href)
         let top = element.offsetTop
 
-        let n = 25
-        let duration = 500/n
+
         let currentTop = window.scrollY
         let targetTop = top - 80
-        let distance = (targetTop - currentTop)/n
-        let i = 0
-        let id = setInterval(() => {
-            if(i === n){
-                window.clearInterval(id)
-                return
-            }
-            i = i + 1
-            window.scrollTo(0, currentTop + distance * i)
-        },duration)
+        let distance = targetTop - currentTop
+        var coords = {y: currentTop }
+        var t = Math.abs((s/100)*300)
+        if (t > 500){ t = 500 }
+        var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+                .to({y: targetTop}, t )// Move to (300, 200) in 1 second.
+                .easing(TWEEN.Easing.Quadratic.InOut) // Use an easing function to make the animation smooth.
+                .onUpdate(function() { // Called after tween.js updates 'coords'.
+                    // Move 'box' to the position described by 'coords' with a CSS translation.
+                window.scrollTo(0,coords.y)
+                })
+                .start(); // Start the tween immediately.
     }
 }
