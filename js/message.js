c267d264ad3e6113d0e3e1ee8904f44b03d0c578
wangsiyuan233 <380191957@qq.com>
2018-03-05 16:55:03 +0800

    眼瞎了

--- js/message.js
+++ js/message.js
@@ -1,38 +1,38 @@
 
-var APP_ID = 'MmCAHWpATYawqMF9T7YhY2VJ-gzGzoHsz';
-var APP_KEY = 'iDt8bccWk3jK69zhK45JbTm7';
+    var APP_ID = 'MmCAHWpATYawqMF9T7YhY2VJ-gzGzoHsz';
+    var APP_KEY = 'iDt8bccWk3jK69zhK45JbTm7';
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-console.log('mnmnm')
+    AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY
+    });
+    console.log('mnmnm')
 
-var query = new AV.Query('Message')
-query.find()
-.then(
-    function(message){
-        let array = message.map((item) => item.attributes)
-        array.forEach((item) => {
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            let messageList = document.querySelector('#messageList')
-            messageList.appendChild(li)
+    var query = new AV.Query('Message')
+    query.find()
+    .then(
+        function(message){
+            let array = message.map((item) => item.attributes)
+            array.forEach((item) => {
+                let li = document.createElement('li')
+                li.innerText = `${item.name}: ${item.content}`
+                let messageList = document.querySelector('#messageList')
+                messageList.appendChild(li)
 
+            })
+        }
+    )
+
+    let myForm = document.querySelector('#postMessageForm')
+    myForm.addEventListener('submit',function(e){
+        e.preventDefault()
+        let content = myForm.querySelector('input[name=content]').value
+        var Message = AV.Object.extend('Message')
+        var message = new Message();
+        message.save({
+            'content':content
+        }).then(function(object){
+            window.location.reload()
+            console.log(object)
         })
-    }
-)
-
-let myForm = document.querySelector('#postMessageForm')
-myForm.addEventListener('submit',function(e){
-    e.preventDefault()
-    let content = myForm.querySelector('input[name=content]').value
-    var Message = AV.Object.extend('Message')
-    var message = new Message();
-    message.save({
-        'content':content
-    }).then(function(object){
-        window.location.reload()
-        console.log(object)
     })
-})
