6a094fc45d822558a8076babfcac98a320e33a5a
wangsiyuan233 <380191957@qq.com>
2018-03-06 10:03:47 +0800

    给简历加个数据库

--- js/message.js
+++ js/message.js
@@ -1,38 +1,75 @@
-
-    var APP_ID = 'MmCAHWpATYawqMF9T7YhY2VJ-gzGzoHsz';
-    var APP_KEY = 'iDt8bccWk3jK69zhK45JbTm7';
-
-    AV.init({
-      appId: APP_ID,
-      appKey: APP_KEY
-    });
-    console.log('mnmnm')
-
-    var query = new AV.Query('Message')
-    query.find()
-    .then(
-        function(message){
-            let array = message.map((item) => item.attributes)
-            array.forEach((item) => {
-                let li = document.createElement('li')
-                li.innerText = `${item.name}: ${item.content}`
-                let messageList = document.querySelector('#messageList')
-                messageList.appendChild(li)
-
+!function(){
+    var view = document.querySelector('section.message')
+    var model = {
+        //获取数据
+        init: function(){
+          var APP_ID = 'TsDnap9SEXjSvGSowP7gXXJC-gzGzoHsz'
+          var APP_KEY = 'rGye31p12mM3wFpNRn9RADu9'
+          AV.init({ appId: APP_ID, appKey: APP_KEY })
+        },
+        fetch: function(){
+            var query = new AV.Query('Message');
+            return query.find() // Promise 对象
+        },
+        save: function(name, content){
+            var Message = AV.Object.extend('Message');
+            var message = new Message();
+            return message.save({  // Promise 对象
+            'name': name,
+            'content': content
             })
         }
-    )
+    }
+    var controller = {
+        view: null,
+        messageList: null,
+        init: function(view,model){
+            this.view = view
+            this.model =model
+            this.initAV()
+            this.loadMessages()
+            this.messageList = view.querySelector('#messageList')
+            this.bindEvents()
+            this.form = view.querySelector('form')
+            this.model.init()
 
-    let myForm = document.querySelector('#postMessageForm')
-    myForm.addEventListener('submit',function(e){
-        e.preventDefault()
-        let content = myForm.querySelector('input[name=content]').value
-        var Message = AV.Object.extend('Message')
-        var message = new Message();
-        message.save({
-            'content':content
-        }).then(function(object){
-            window.location.reload()
+        },
+
+        loadMessages: function(){
+            this.model.fetch().then(
+                (message =>{
+                    let array = message.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.appendChild(li)
+
+                    })
+                }
+            )
+        },
+
+        bindEvents: function(){
+          this.form.addEventListener('submit', function(e){
+            e.preventDefault()
+            this.saveMessage()
+          })
+        },
+
+        saveMessage: function(){
+          let myForm = this.form
+          let content = myForm.querySelector('input[name=content]').value
+          let name = myForm.querySelector('input[name=name]').value
+          this.model.save(name, content).then(function(object) {
+            let li = document.createElement('li')
+            li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+            let messageList = document.querySelector('#messageList')
+            messageList.appendChild(li)
+            myForm.querySelector('input[name=content]').value = ''
             console.log(object)
-        })
-    })
+          })
+        }
+    }
+    controller.init(view, model)
+
+}.call()
